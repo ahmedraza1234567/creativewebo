d68dcf4d41bd88e1314ab2867330700c15cab5a3
Ahmed Raza Shaikh <114650047+ahmedraza1234567@users.noreply.github.com>
2026-03-25 18:47:10 +0530

    Update database.py

--- backend/database.py
+++ backend/database.py
@@ -1,5 +1,24 @@
-from pymongo import MongoClient
+# from pymongo import MongoClient
 
-client = MongoClient("mongodb://localhost:27017/")
-db = client["taskdb"]
-collection = db["users"]
+# client = MongoClient("mongodb://localhost:27017/")
+# db = client["taskdb"]
+# collection = db["users"]
+
+
+
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+
+uri = "mongodb+srv://raza10541054:Password%40hmed1054@creativewebotest.tjufzxw.mongodb.net/"
+
+# Create a new client and connect to the server
+client = MongoClient(uri, server_api=ServerApi('1'))
+
+# Send a ping to confirm a successful connection
+try:
+    client.admin.command('ping')
+    db = client["taskdb"]
+    collection = db["users"]
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
